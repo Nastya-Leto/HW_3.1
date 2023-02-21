17b9880baddab78f352dc9c0519a624cae748d1e
Anastasiya <soidberg91@gmail.com>
2023-02-21 09:48:34 +0400

    test form Registration

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,40 +1,59 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 
 public class FormTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+
     }
 
     @Test
     void fillFormTest() {
         open("https://demoqa.com/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
 
 
-
-       $("[id=firstName]").setValue("Z");
-       $("[id=lastName]").setValue("Anastasiya");
-       $("[id=userEmail]").setValue("stasia-oops@yandex.ru");
-
-        //$(By.name("me")).selectRadio("cat"); - Как использовать
+        $("[id=firstName]").setValue("Z");
+        $("[id=lastName]").setValue("Anastasiya");
+        $("[id=userEmail]").setValue("stasia-oops@yandex.ru");
         $("#gender-radio-1").sendKeys(" ");
-               $("[id=userNumber]").setValue("8927111111");
-        $("#hobbies-checkbox-1").sendKeys(" ");
+        $("[id=userNumber]").setValue("8927111111");
 
-      //  $("[id=userNumber]").setValue("Samara");
-        //$("#submit").click();
-        //$("[id=output]").shouldHave(text("Anastasiya Z"),text("stasia-oops@yandex.ru"),
-            //    text("Sochi"),text("Samara"));
+        $(".react-datepicker-wrapper").click();
+        $(".react-datepicker__year-select").$(byText("1991")).click();
+        $(".react-datepicker__month-select").$(byText("June")).click();
+        $(".react-datepicker__day.react-datepicker__day--027").click();
+
+        $("#subjectsInput").setValue("Arts").pressEnter();
+        $("#hobbies-checkbox-1").sendKeys(" ");
+        $("#uploadPicture").uploadFromClasspath("pictures/img1.png");
+        $("[id=currentAddress]").setValue("SamaraSamaraSamaraSamaraSamaraSamaraSamara");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
+        $("#submit").click();
+
+
+        $(".table-responsive").shouldHave(
+                text("Z Anastasiya"),
+                text("stasia-oops@yandex.ru"),
+                text("Male"),
+                text("8927111111"),
+                text("27 May,1991"),
+                text("Arts"),
+                text("Sports"),
+                text("img1.png"),
+                text("SamaraSamaraSamaraSamaraSamaraSamaraSamara"),
+                text("NCR Noida"));
     }
 }
 
